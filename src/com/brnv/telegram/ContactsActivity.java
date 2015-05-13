24e6a7c74195db4d07caa86940cd82e3b4a2e5f3
Alexey Baranov <a.baranov@office.ngs.ru>
2015-05-13 15:53:48 +0600

    contacts add dummy

--- src/com/brnv/telegram/ContactsActivity.java
+++ src/com/brnv/telegram/ContactsActivity.java
@@ -16,19 +16,27 @@ import android.view.MenuItem;
 
 import org.drinkless.td.libcore.telegram.*;
 
+import android.widget.ViewFlipper;
+
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.EditText;
+
 public class ContactsActivity extends Activity {
 
     public static ContactsActivity instance;
 
+    public ViewFlipper viewFlipper;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts);
-        getActionBar().setTitle("Contacts");
 
         instance = this;
+        viewFlipper = (ViewFlipper) findViewById(R.id.contacts_views);
 
         TdApiResultHandler.getInstance().Send(new TdApi.GetContacts());
     }
@@ -44,7 +52,10 @@ public class ContactsActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
          switch (item.getItemId()) {
         case R.id.contacts_add:
-            Log.v("!!!", "add contact");
+            ContactsActivity.instance.AddContact();
+            return true;
+        case android.R.id.home:
+            TdApiResultHandler.getInstance().Send(new TdApi.GetContacts());
             return true;
         default:
             return super.onOptionsItemSelected(item);
@@ -56,6 +67,43 @@ public class ContactsActivity extends Activity {
         this.finish();
     }
 
+    public void AddContact() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                getActionBar().setTitle("");
+                getActionBar().setDisplayHomeAsUpEnabled(true);
+                ContactsActivity.instance.viewFlipper.setDisplayedChild(1);
+            }
+        });
+
+        Button
+            addContactButton = (Button) findViewById(R.id.button_add_contact);
+
+        addContactButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                v.setEnabled(false);
+
+                EditText
+                    firstNameInput = (EditText) findViewById(R.id.input_contact_first_name);
+
+                String firstName = firstNameInput.getText().toString();
+
+                EditText
+                    lastNameInput = (EditText) findViewById(R.id.input_contact_last_name);
+
+                String lastName = lastNameInput.getText().toString();
+
+                EditText
+                    phoneInput = (EditText) findViewById(R.id.input_contact_phone);
+
+                String phone = phoneInput.getText().toString();
+
+                Log.v("!!! add contact", firstName + " " + lastName + " " + phone);
+            }
+        });
+    }
+
     public void ListContacts(TdApi.Contacts contacts) {
         final TextView product = new TextView(this);
         product.setText("test");
@@ -63,6 +111,11 @@ public class ContactsActivity extends Activity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                getActionBar().setTitle("Contacts");
+                getActionBar().setDisplayHomeAsUpEnabled(false);
+
+                ContactsActivity.instance.viewFlipper.setDisplayedChild(0);
+
                 LinearLayout
                     rootLayout = (LinearLayout) findViewById(R.id.layout_contacts);
 
